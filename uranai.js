0eb6ec45efc92f9b183cfa059575fbb3f5388986
Tsuji-Tomo <144186328+Tsuji-Tomo@users.noreply.github.com>
2023-10-09 13:47:41 +0900

    Update uranai.js

--- uranai.js
+++ uranai.js
@@ -1,18 +1,28 @@
 "use strict";
 // // 1行目に記載している 'use strict' は削除しないでください
 
-console.log("繋がったよ！")
-
-
 //  ----------------------------------------------------------
 // 　今日の運勢
 //  ----------------------------------------------------------
 
+
 function uranai() {
   // console.log("🎃");
-  let unsei = ["🧡🧡🧡🧡🧡", "💛💛💛💛", "💚💚💚", "💙💙", "💜", "💔"];
-  let i = Math.floor( Math.random() * unsei.length) ;
-  document.getElementsByClassName("bluebox")[0].innerText = unsei[i];
+  let health = ["🧡🧡🧡🧡🧡", "🧡🧡🧡🧡", "🧡🧡🧡", "🧡🧡", "🧡", "💔"];
+  let i = Math.floor( Math.random() * health.length) ;
+  document.getElementsByClassName("health")[0].innerText = health[i];
+
+  let money = ["💴💴💴💴💴", "💴💴💴💴", "💴💴💴", "💴💴", "💴", "💸"];
+  let j = Math.floor( Math.random() * money.length) ;
+  document.getElementsByClassName("money")[0].innerText = money[j];
+
+  let work = ["😀😀😀😀😀", "🙂🙂🙂🙂", "😶😶😶", "😵😵", "😱", "🤮", "💀"];
+  let k = Math.floor( Math.random() * work.length) ;
+  document.getElementsByClassName("work")[0].innerText = work[k];
+
+  document.getElementsByClassName("comment")[0].innerText = "Have a wonderful day!!";
+
+  document.getElementById("gif").innerHTML =  '<img src="https://usagif.com/wp-content/uploads/gify/barbie-animated-usagif-22.gif">';  
 }
 
 const target = document.getElementsByClassName("button")[0];
